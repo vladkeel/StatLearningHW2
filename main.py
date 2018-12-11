63ab67dba1658b113b76258e84c4f578162aeab7
vrkeel <vladkeel@gmail.com>
2018-12-11 20:05:31 +0200

    fixes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from utils import load_mnist
+from utils import progress_bar
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_breast_cancer
 
 class Perceptron(object):
 
-    def __init__(self, data, threshold=100):
+    def __init__(self, data, threshold=1000000):
         # Threshold , number of epochs that algorithm will do, default = 100
         self.epochs = threshold
 
@@ -16,17 +18,21 @@ class Perceptron(object):
 
         # TODO: Check if this (len(data[0])) approach is working
         # Number of columns in dataframe
-        self.num_of_columns = len(data['data'])
+        self.num_of_columns = len(data['data'][0])
 
         # Inputs (X)
         # Assumption: n-1 columns of data frame contains input data vectors
         # self.X = self.data.iloc[:, :(self.num_of_columns - 1)]
-        self.X = self.data['data']
+        self.X = []
+        for i in range(len(self.data['data'])):
+            self.X.append(np.append(self.data['data'][i], [1]))
 
         # Labels (Y)
         # Assumption: A column n`th of data frame is the labels vector
         # self.Y = self.data.iloc[:, :-1]
-        self.Y = self.data['target']
+        self.Y = []
+        for i in range(len(self.data['target'])):
+            self.Y.append(1 if self.data['target'][i] == 1 else -1)
 
         # Weights vector
         # TODO: Calculate right value
@@ -34,13 +40,29 @@ class Perceptron(object):
 
     def train(self):
         # Algorithm implementation
+        progress_bar(0, "Training perceptron...")
         for t in range(self.epochs):
+            progress_bar(t / self.epochs, "Training perceptron...")
             for i, x in enumerate(self.X):
                 if (np.dot(self.X[i], self.weights) * self.Y[i]) <= 0:
                     # TODO: Does it update weights vector at index i, ot should we use self.weights[i]
                     self.weights = self.weights + self.X[i] * self.Y[i]
-
+                    break
+            else:
+                break
+        else:
+            progress_bar(1, "Training perceptron completed without convergence")
+        print("Perceptron ready")
         return self.weights
+
+    def predict(self, x):
+        return 1 if np.dot(np.append(x, [1]), self.weights) > 0 else -1
+
+    def predict_arr(self, X):
+        res = []
+        for i in range(len(X)):
+            res.append(self.predict(X[i]))
+        return res
 
 
 def func(x):
@@ -76,10 +98,9 @@ def ex3b(model, x_train, y_train, x_test, y_test):
 def ex4(data):
     perceptron = Perceptron(data)
     w = perceptron.train()
-    print(w)
+    return w
 
-
-if __name__ == '__main__':
+def ex2():
     # Exercise 2:
     # Show function plot:
     X = np.arange(-5, 5, 0.1)
@@ -98,10 +119,14 @@ if __name__ == '__main__':
         x_step = updt(grad, x, 1e-3)
     print("Function minimum is at ({}, {})".format(x_step, func(x_step)))
 
+
+if __name__ == '__main__':
+    ex2()
     # Exercise 3:
     data_df, labels_df = load_mnist()
     X_train, X_test, y_train, y_test = train_test_split(data_df, labels_df, random_state=98, test_size=0.143)
     clf = ex3a(X_train, y_train)
     ex3b(clf, X_train, y_train, X_test, y_test)
+    ex4(load_breast_cancer())
 
 
--- utils.py
+++ utils.py
@@ -2,7 +2,22 @@ import requests
 import pandas as pd
 
 from sklearn.datasets import fetch_mldata
+import sys
 
+def progress_bar(progress, text):
+    """
+    Prints progress bar to console
+    Args:
+        progress: float in [0,1] representing progress in action
+                    where 0 nothing done and 1 completed.
+        text: Short string to add after progress bar.
+    """
+    if isinstance(progress, int):
+        progress = float(progress)
+    block = int(round(20*progress))
+    progress_line = "\rCompleted: [{0}] {1:5.2f}% {2}.".format("#"*block + "-"*(20-block), progress*100, text)
+    sys.stdout.write(progress_line)
+    sys.stdout.flush()
 
 def load_mnist():
     try:
